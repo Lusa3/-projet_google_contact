08a3f729d75e3a25d6bf60457479366796a246ed
Grace Luswanga <luswanga2@gmail.com>
2024-05-13 23:10:17 +0100

    créer un formulaire et afficher son contenu

--- index.js
+++ index.js
@@ -29,3 +29,38 @@ onglets.forEach(function(onglet, index) {
 });
 // Activer le premier onglet par défaut
 ongletActive(0);
+
+
+// let firstname = document.getElementById('#firstname');
+// let name = document.getElementById('#name').value;
+// let company = document.getElementById('#company').value;
+// let post = document.getElementById('#post').value;
+// let email = document.getElementById('#email').value;
+// let phone = document.getElementById('#phone').value;
+
+const formulaire = document.getElementById('formulaire-contact');
+
+
+formulaire.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    
+    const firstname = document.getElementById('firstname').value;
+    const name = document.getElementById('name').value;
+    const company = document.getElementById('company').value;
+    const post = document.getElementById('post').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+
+  
+    document.getElementById('resultat_firstname').textContent = firstname;
+    document.getElementById('resultat_name').textContent = name;
+    document.getElementById('resultat_company').textContent = company;
+    document.getElementById('resultat_post').textContent = post;
+    document.getElementById('resultat_email').textContent = email;
+    document.getElementById('resultat_phone').textContent = phone;
+    
+    const resultatDiv = document.getElementById('resultat');
+
+    resultatDiv.innerHTML = resultatHtml;
+});
